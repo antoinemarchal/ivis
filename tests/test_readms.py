d2275deb6387ccf821a0d81cd90c438db53ea099
antoinemarchal <amarchal@cita.utoronto.ca>
2025-03-20 09:52:44 +1100

    clean test_readms.py

--- tests/test_readms.py
+++ tests/test_readms.py
@@ -1,26 +1,7 @@
 # -*- coding: utf-8 -*-
-import numpy as np
-from astropy import units as u
-from astropy.constants import c
-import matplotlib.pyplot as plt
-from astropy.io import fits
-from astropy import wcs
-from astropy.coordinates import SkyCoord
-from radio_beam import Beam
-import torch
-from tqdm import tqdm as tqdm
-import queue  # Standard Python queue for passing data between threads
-import threading
-import multiprocessing
-import time
-
 from deconv.core import DataVisualizer, DataProcessor, Imager
 from deconv.utils import dutils
 from deconv import logger
-
-import marchalib as ml #remove
-
-plt.ion()
 
 if __name__ == '__main__':    
     #path data
