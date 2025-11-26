ba1479e911c85371e867d896baa8b47b5e5d6dfc
antoinemarchal <amarchal@cita.utoronto.ca>
2025-11-26 23:23:38 +1100

    safe untar

--- examples/untar.py
+++ examples/untar.py
@@ -36,5 +36,5 @@ if __name__ == '__main__':
     
     #PRE-COMPUTE DATA
     #untardir and fixms
-    data_processor.untardir(max_workers=56, clear=True) #warning clean=True will clear the .tar files
+    data_processor.untardir(max_workers=56, clear=False) #warning clean=True will clear the .tar files
 
