dc9578ed6ff4da4591fc711d0a333faa0fd6ce34
antoinemarchal <amarchal@cita.utoronto.ca>
2026-03-21 12:53:51 +0100

    Simplify package initialization

--- ivis/__init__.py
+++ ivis/__init__.py
@@ -1,11 +1,9 @@
 """
 IViS — Interferometric Visibility-space Inversion Software.
 """
+
 __version__ = "0.1.0"
 
-from . import imager
-from . import io
-from . import pipeline
-from . import utils
-
 from .logger import logger
+
+__all__ = ["__version__", "logger"]
