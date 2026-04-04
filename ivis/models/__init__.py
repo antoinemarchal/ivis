b64d021b8014b1cda10f1f68d743acd1433ef76c
antoinemarchal <amarchal@cita.utoronto.ca>
2026-04-04 16:26:26 +0200

    Handle optional model backends

--- ivis/models/__init__.py
+++ ivis/models/__init__.py
@@ -1,4 +1,22 @@
-from .classic import ClassicIViS, ClassicIViS3D
 from .classic3D import Classic3D
 from .classic3D_analytical import Classic3DAnalytical
-from .twist import TWiSTModel
+
+__all__ = [
+    "Classic3D",
+    "Classic3DAnalytical",
+]
+
+try:
+    from .classic import ClassicIViS, ClassicIViS3D
+except ModuleNotFoundError:
+    ClassicIViS = None
+    ClassicIViS3D = None
+else:
+    __all__.extend(["ClassicIViS", "ClassicIViS3D"])
+
+try:
+    from .twist import TWiSTModel
+except ModuleNotFoundError:
+    TWiSTModel = None
+else:
+    __all__.append("TWiSTModel")
