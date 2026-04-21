46a8a15c2b960ce5329d2e55f13c57559bfe7608
antoinemarchal <amarchal@cita.utoronto.ca>
2026-04-21 09:30:59 +0200

    clean more models

--- ivis/models/__init__.py
+++ ivis/models/__init__.py
@@ -1,14 +1,7 @@
 from .classic3D import Classic3D
-from .classic3D_cached import Classic3DCached
-from .classic3D_analytical import Classic3DAnalytical
-from .classic import ClassicIViS, ClassicIViS3D
 from .lrsb import LRSB
 
 __all__ = [
     "Classic3D",
-    "Classic3DCached",
-    "Classic3DAnalytical",
-    "ClassicIViS",
-    "ClassicIViS3D",
     "LRSB",
 ]
